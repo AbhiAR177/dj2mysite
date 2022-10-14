a525edec14fd5cbb78f6128e326f2d2398eac026
AbhiAR177 <abhijithar177@gmail.com>
2022-10-14 14:38:15 +0530

    second  commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,8 @@
+from hashlib import new
 from django.shortcuts import HttpResponse, render
 
 def index(request):
     return HttpResponse(['string',1,2,3,4])
+
+def new_one(request):
+    return HttpResponse("this is new")
